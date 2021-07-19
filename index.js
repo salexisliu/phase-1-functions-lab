a7bd18c67372ffc892d927d62f4da999677a5382
Alexis S Liu <syl1350@gmail.com>
2021-07-19 16:03:09 -0400

    commit tree 4ad25ecb36d22dc74d36f7cbc890672e8cdcf3f5

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
-// Code your solution in this file!
+
+function distanceFromHqInBlocks(pickupLocation){
+let startLocation = 42;
+let blocks = Math.abs(pickupLocation - startLocation);
+return blocks;
+}
+
+function distanceFromHqInFeet(value) {
+  return distanceFromHqInBlocks(value) * 264
+}
+
+function distanceTravelledInFeet(start, end){
+  return Math.abs(end - start ) * 264
+}
+
+function calculatesFarePrice(start, end) {
+  let feet = (distanceTravelledInFeet(start, end)) 
+  if (feet > 2500) {
+    return 'cannot travel that far'
+   } else if (feet > 400 && feet < 2000) {
+    return ((feet - 400) * .02)
+   } else if (feet > 2000) {
+    return 25
+   } else {
+    return 0
+   }
+  }
+  
